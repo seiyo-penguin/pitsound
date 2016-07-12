dd37eaeb50b1e0e5367a21f265cfe3bea5a9d281
初コミット <Seiyo@LiuXingyu-no-MacBook-Air.local>
2016-07-12 20:57:33 +0900

    ユーザー登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_out_path_for(resource)
+      '/' # サインアウト後のリダイレクト先URL
+    end
+
+    def after_sign_up_path_for(resource)
+      '/users/:id'
+    end
+
+def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up).push(:username, :avatar)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: :show
+  root 'main#index'
   resources :main do
+    # get 'main' => 'main#index'
     resources :music do
+
       resources :comments do
+
       end
     end
   end
