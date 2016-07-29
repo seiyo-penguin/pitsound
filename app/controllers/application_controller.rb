089ef258739e5cee36a154381bf9dd48df747d45
初コミット <Seiyo@LiuXingyu-no-MacBook-Air.local>
2016-07-29 22:46:21 +0900

    change user sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
     end
 
 def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up).push(:username, :avatar)
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
     end
 end
